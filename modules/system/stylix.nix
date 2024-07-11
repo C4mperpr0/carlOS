f9f0ed49a603dbdb632a921cee72601e07ce03cd
Carl Bellgardt <carlbellgardt@gmail.com>
2024-07-11 21:25:50 +0200

    updated pretty much everything

---
+++ modules/system/stylix.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  pkgs,
+  flake-confs,
+  pkgs-unstable,
+  inputs,
+  config,
+  ...
+}: let
+  cfg = config.nixosModules.office;
+in {
+  options = {
+    nixosModules.office = {
+      enable = lib.mkEnableOption "enable office programs";
+      latex.enable = lib.mkEnableOption "enable latex";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    programs = {
+      kdeconnect.enable = true;
+      droidcam.enable = true;
+    };
+
+    boot = {
+      kernelModules = ["v4l2loopback"]; # for droidcam
+      extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+    };
+    environment.systemPackages = with pkgs;
+      [
+        firefox
+        #pkgs-unstable.obsidian
+        obsidian
+        kate
+        okular
+        thunderbird
+        libreoffice
+        bitwarden
+        wacomtablet
+        speedcrunch
+        linuxKernel.packages.linux_6_1.v4l2loopback
+        android-tools
+      ]
+      ++ lib.optionals cfg.latex.enable
+      [
+        texliveFull
+        jabref
+        texstudio
+      ];
+  };
+}
