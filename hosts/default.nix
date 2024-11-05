09dac7247d0c90fd74a027ffe06a59db901c9b15
Carl Heinrich Bellgardt <carlbellgardt@gmail.com>
2024-11-05 09:23:08 +0100

    create extra lenovo yoga config with media editing tools enabled

--- hosts/default.nix
+++ hosts/default.nix
@@ -19,6 +19,7 @@
       inputs.stylix.nixosModules.stylix
   ];
 in {
+  # universal laptop config
   laptop = lib.nixosSystem {
     inherit system;
     specialArgs = {
@@ -29,6 +30,20 @@ in {
       ./laptop
     ];
   };
+
+  # specific lenovo yoga pro 7 gen 9 config
+  laptop-lenovo-yoga = lib.nixosSystem {
+    inherit system;
+    specialArgs = {
+      inherit inputs flake-confs pkgs pkgs-unstable;
+      buildName = "laptop-lenovo-yoga";
+    };
+    modules = commonModules ++ [
+      ./laptop-lenovo-yoga
+    ];
+  };
+
+  # universal desktop pc config
   pc = lib.nixosSystem {
     inherit system;
     specialArgs = {
