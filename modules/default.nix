07ea8fe1d4e38e769f51e61ae4dd3697cfb76b76
Carl Heinrich Bellgardt <carlbellgardt@gmail.com>
2025-03-27 13:59:59 +0100

    removed obsolete lines

--- modules/default.nix
+++ modules/default.nix
@@ -1,20 +1,8 @@
 {
   inputs,
-  flake-confs,
   ...
-}: let
-  system = flake-confs.system;
-
-  pkgs = import inputs.nixpkgs {
-    inherit system;
-    config.allowUnfree = true;
-  };
-  pkgs-unstable = import inputs.nixpkgs-unstable {
-    inherit system;
-    config.allowUnfree = true;
-  };
-  lib = inputs.nixpkgs.lib;
-in {
+}: 
+{
   modules = [
     ./system
     inputs.home-manager.nixosModules.home-manager
