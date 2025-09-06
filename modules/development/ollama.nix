af05d94fcc544a9ffff024f248570c6c56bf25ca
cabe7261 <cabe7261@th-wildau.de>
2025-09-06 12:40:16 +0200

    fixed very bad typo

--- modules/development/ollama.nix
+++ modules/development/ollama.nix
@@ -4,7 +4,7 @@
   pkgs-unstable,
   ...
 }: let
-  cfg = config.nixosModules.carlOS.development.programming;
+  cfg = config.nixosModules.carlOS.development.ollama;
 in {
   options = {
     nixosModules.carlOS.development.ollama = {
